349657669c8440b5cc693b12c38b0eacf4e5caa1
Levente Dámsa <damsalevente@gmail.com>
2016-08-22 13:10:22 +0200

    Testing maxvalue reducer

--- MapReduce/Test.cpp
+++ MapReduce/Test.cpp
@@ -4,39 +4,16 @@ int main()
 {
 	using namespace std;
 	using namespace MapReduce;
-	KeyValuePair<int, string> firstElement(1950, "Yes");
-	KeyValuePair<int, string> secondElement(1950, "NO");
-	KeyValuePair<int, string> thirdElement(1933, "MAYBE");
-	firstElement.printOut();
-	Map<int, string> myMap;
-	myMap.add(firstElement);
-	myMap.add(secondElement);
-	myMap.add(thirdElement);
-	vector<int> tomb;
-	tomb=myMap.getKeys();
-	for (auto it : tomb)
+	Map<int, double> mapper;
+	vector<KeyValuePair<int, double>> inputFileSimulator;
+	for (int i = 0; i < 100; i++)
 	{
-		cout << it << endl;;
+		inputFileSimulator.push_back(KeyValuePair<int, double>(i%2, sin(i)));
+		mapper.add(inputFileSimulator[i]);
 	}
-	vector<string> another;
-	another = myMap.getVectorWithKey(1950);
-	cout << "1950 elements:" <<  endl;
-	for (auto it : another)
-	{
-		cout << it << endl;
-	}
-	myMap.map();
-	
-	myMap.getListWithIndex(0).printResult();
-	myMap.getListWithIndex(1).printResult();
-	
-	KeyValueList<int,string> *result=myMap.getEverything();
-	for (int i=0;i<2;i++)
-	{
-		result[i].printResult();
-	}
-	Reduce<int, string> redo(myMap.getResult());
-	redo.reduceMax();
-	redo.printOut();
+	mapper.map();
+	Reduce<int, double> reducer(mapper.getResult());
+	reducer.reduceMax();
+	reducer.printOut();
 	return 0;
 }
