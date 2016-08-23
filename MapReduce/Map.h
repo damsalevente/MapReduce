370a04af852a470333462554ba3a58c13cda29f7
Levente Dámsa <damsalevente@gmail.com>
2016-08-23 11:31:32 +0200

    File handling added

--- MapReduce/Map.h
+++ MapReduce/Map.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include<fstream>
 #include <limits>
 using namespace std;
 
@@ -19,6 +20,8 @@ namespace MapReduce
 		Value getValueAt(int i);
 		vector<Value> getAllValues();
 		Value getMax();
+		Value getSum();
+		Value getMin();
 		void printResult()const;
 	};
 	template<class Key, class Value>
@@ -29,6 +32,7 @@ namespace MapReduce
 	public:
 		KeyValuePair() {};
 		KeyValuePair(Key k, Value v) :key(k), value(v) {};
+		KeyValuePair(string line);
 		void addKV(Key k, Value v);
 		void printOut()const;
 		Key getKey()const;
@@ -36,6 +40,12 @@ namespace MapReduce
 
 
 	};
+	template<class Key, class Value>
+	inline KeyValuePair<Key, Value>::KeyValuePair(string line)
+	{
+		this->key = atoi(line.substr(0, line.find(',')).c_str());
+		this->value = line.substr(line.find(',') + 1);
+	}
 	template<class Key, class Value>
 	inline void KeyValuePair<Key, Value>::addKV(Key k, Value v)
 	{
@@ -46,7 +56,7 @@ namespace MapReduce
 	template<class Key, class Value>
 	inline void KeyValuePair<Key, Value>::printOut() const
 	{
-		cout << this->key << " " << this->value << endl;
+		cout <<"Key: "<< this->key << " \t" <<"Value: "<< this->value << endl;
 	}
 
 	template<class Key, class Value>
@@ -69,6 +79,7 @@ namespace MapReduce
 		//this is what i will get, Value* must be inicialised before passing the Value
 		vector<KeyValueList<Key,Value>> result;
 	public:
+		void add(string fileName);
 		void add( KeyValuePair<Key,Value>  kvp);
 		void remove();
 		vector<Value> getVectorWithKey(Key k);
@@ -86,12 +97,31 @@ namespace MapReduce
 		vector<KeyValuePair<Key, Value>> reduceOutput;
 	public:
 		Reduce(vector<KeyValueList<Key, Value>> kvp) :mapInput(kvp) {};
+		void reduceSum();
 		void reduceMax();
+		void reduceMin();
 		void printOut()const;
 
 	};
 
 	
+
+	template<class Key, class Value>
+	inline void Map<Key, Value>::add(string fileName)
+	{
+		ifstream fileStream(fileName);
+		if (fileStream.is_open())
+		{
+			string cim;
+			getline(fileStream,cim);
+			string line;
+			while (getline(fileStream, line,'\n'))
+			{
+				KeyValuePair<Key, Value> currentLine(line);
+				this->source.push_back(currentLine);
+			}
+		}
+	}
 
 	template<class Key, class Value>
 	inline void Map<Key, Value>::add( KeyValuePair<Key,Value> kvp)
@@ -205,6 +235,29 @@ namespace MapReduce
 	}
 
 	template<class Key, class Value>
+	inline Value KeyValueList<Key, Value>::getSum()
+	{
+		Value sum = 0;
+		for (auto it = this->values.cbegin(); it != values.cend(); ++it)
+		{
+			sum += *it;
+		}
+		return sum;
+	}
+
+	template<class Key, class Value>
+	inline Value KeyValueList<Key, Value>::getMin()
+	{
+		Value min = this->values[0];
+		for (auto it = values.cbegin(); it != values.cend(); ++it)
+		{
+			if (min > *it)
+				min = *it;
+		}
+		return min;
+	}
+
+	template<class Key, class Value>
 	inline void KeyValueList<Key, Value>::printResult() const
 	{
 		cout << "---------------------------------------" << endl;
@@ -219,23 +272,47 @@ namespace MapReduce
 		cout << "---------------------------------------" << endl;
 	}
 	template<class Key, class Value>
-	inline void Reduce<Key, Value>::reduceMax()
+	inline void Reduce<Key, Value>::reduceSum()
 	{
+		reduceOutput.clear();
 		for (int i = 0; i < mapInput.size(); i++)
 		{
 			KeyValueList<Key, Value> currentKey(mapInput.at(i));
-			
+			KeyValuePair<Key, Value> currentResult(currentKey.getKey(), currentKey.getSum());
+			reduceOutput.push_back(currentResult);
+		}
+	}
+	template<class Key, class Value>
+	inline void Reduce<Key, Value>::reduceMax()
+	{
+		reduceOutput.clear();
+		for (int i = 0; i < mapInput.size(); i++)
+		{
+			KeyValueList<Key, Value> currentKey(mapInput.at(i));
 			KeyValuePair<Key, Value> currentResult(currentKey.getKey(), currentKey.getMax());
+			reduceOutput.push_back(currentResult);
+		}
+	}
+	template<class Key, class Value>
+	inline void Reduce<Key, Value>::reduceMin()
+	{
+		reduceOutput.clear();
+		for (int i = 0; i < mapInput.size(); i++)
+		{
+			KeyValueList<Key, Value> currentKey(mapInput.at(i));
+			KeyValuePair<Key, Value> currentResult(currentKey.getKey(), currentKey.getMin());
 			reduceOutput.push_back(currentResult);
 		}
 	}
 	template<class Key, class Value>
 	inline void Reduce<Key, Value>::printOut() const
 	{
+		cout << "============================" << endl;
 		for (int i = 0; i < reduceOutput.size(); i++)
 		{
 			KeyValuePair<Key,Value> current(reduceOutput.at(i));
 			current.printOut();
 		}
+		cout << "============================" << endl;
 	}
 }
--- MapReduce/Test.cpp
+++ MapReduce/Test.cpp
@@ -8,12 +8,25 @@ int main()
 	vector<KeyValuePair<int, double>> inputFileSimulator;
 	for (int i = 0; i < 100; i++)
 	{
-		inputFileSimulator.push_back(KeyValuePair<int, double>(i%2, sin(i)));
+		inputFileSimulator.push_back(KeyValuePair<int, double>(i%3, sin(i)));
 		mapper.add(inputFileSimulator[i]);
 	}
 	mapper.map();
 	Reduce<int, double> reducer(mapper.getResult());
 	reducer.reduceMax();
 	reducer.printOut();
+	cout << "Minimum " << endl;
+	reducer.reduceMin();
+	reducer.printOut();
+	cout << "Sum" << endl;
+	reducer.reduceSum();
+	reducer.printOut();
+
+	Map<int, string> Cica;
+	Cica.add("catsM.csv");
+	Cica.map();
+	Reduce<int, string> CicaReduce(Cica.getResult());
+	CicaReduce.reduceMax();
+	CicaReduce.printOut();
 	return 0;
 }
